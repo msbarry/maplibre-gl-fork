dab59eda79c7d40ce2a6733cf89afae34fd1ecda
Mike Barry <msb5014@gmail.com>
2025-03-12 20:45:04 -0400

    setup

--- push.sh
+++ push.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-TAG="${1?Please specify a tag}"
+npm version prerelease
 ./build.sh
-git add -A .
-git commit -m "build ${TAG}"
-git tag ${TAG}
-git push fork ${TAG} --force
-git push fork `git rev-parse --abbrev-ref HEAD` --force
+npm publish
